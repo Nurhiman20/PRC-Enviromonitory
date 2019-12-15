cab07bbbc79691d585fb2c89c8213e37eda03a68
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2019-12-15 13:15:16 +0700

    Use props for Chart component

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -8,49 +8,85 @@
 
 <script>
 export default {
+    props: ['chartData'],
     data() {
         return {
-            options: options
+            options: {
+                        title: {
+                            text: this.chartData[0].title,
+                            x: -20 //center
+                        },
+                        subtitle: {
+                            text: "Source: NodeMCU Device",
+                            x: -20
+                        },
+                        xAxis: {
+                            categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+                        },
+                        yAxis: {
+                            title: {
+                                text: this.chartData[0].yAxisTitle
+                            },
+                            plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                        },
+                        tooltip: {
+                            valueSuffix: this.chartData[0].valueSuffix
+                        },
+                        // legend: {
+                        //     layout: 'vertical',
+                        //     align: 'right',
+                        //     verticalAlign: 'middle',
+                        //     borderWidth: 0
+                        // },
+                        series: [{
+                            name: 'Temperature',
+                            data: this.chartData[0].dataSensor
+                        }]
+            }
         }
     }
 }
 
-var options = {
-    title: {
-        text: 'Temperature Chart',
-        x: -20 //center
-    },
-    subtitle: {
-        text: "Source: NodeMCU Device",
-        x: -20
-    },
-    xAxis: {
-        categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-    },
-    yAxis: {
-        title: {
-            text: 'Temperature (°C)'
-        },
-        plotLines: [{
-            value: 0,
-            width: 1,
-            color: '#808080'
-        }]
-    },
-    tooltip: {
-        valueSuffix: '°C'
-    },
-    // legend: {
-    //     layout: 'vertical',
-    //     align: 'right',
-    //     verticalAlign: 'middle',
-    //     borderWidth: 0
-    // },
-    series: [{
-        name: 'Temperature',
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3]
-    }]
-};
+// var options = {
+//     title: {
+//         text: 'Temperature Chart',
+//         x: -20 //center
+//     },
+//     subtitle: {
+//         text: "Source: NodeMCU Device",
+//         x: -20
+//     },
+//     xAxis: {
+//         categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+//     },
+//     yAxis: {
+//         title: {
+//             text: 'Temperature (°C)'
+//         },
+//         plotLines: [{
+//             value: 0,
+//             width: 1,
+//             color: '#808080'
+//         }]
+//     },
+//     tooltip: {
+//         valueSuffix: '°C'
+//     },
+//     // legend: {
+//     //     layout: 'vertical',
+//     //     align: 'right',
+//     //     verticalAlign: 'middle',
+//     //     borderWidth: 0
+//     // },
+//     series: [{
+//         name: 'Temperature',
+//         data: []
+//     }]
+// };
 </script>
 
 <style lang="scss" scoped>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <app-parameter></app-parameter>
         <app-dropdown></app-dropdown>
-        <app-chart></app-chart>
+        <app-chart :chartData="chartData"></app-chart>
     </div>
 </template>
 
@@ -12,6 +12,17 @@
     import Dropdown from './Dropdown.vue'
 
     export default {
+        data() {
+            return {
+                // chartData: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3]
+                chartData: [{ 
+                    title: 'Temperature Chart', 
+                    yAxisTitle: 'Temperature (°C)', 
+                    valueSuffix: '°C', 
+                    dataSensor: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3]
+                }]
+            }
+        },
         components: {
             appParameter: Parameter,
             appChart: Chart,
