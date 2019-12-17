d3196537a21ebc7c50b08a4dcd0bc4f4920e21d1
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2019-12-17 15:43:36 +0700

    Delete data.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -14,7 +14,6 @@
     export default {
         data() {
             return {
-                selectedParameter: 2,
                 parameters: [{ 
                         name: 'Temperature',
                         icon: 'fas fa-temperature-low', 
